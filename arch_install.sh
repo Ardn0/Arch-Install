21ae8679e29fd4b66e510b58af58b4624c01694f
Ardn0 <holomek.o@gmail.com>
2024-05-25 15:59:43 +0200

    Initial commit

---
+++ arch_install.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+set -e # Exit if any command fail
+
+DISK=($(ls /dev/ | grep -E 'nvme[0-9]+n[0-9]$'))
+DISK_SIZE=()
+
+for device in ${DISK[@]}; do
+	size=$(lsblk -dn -o NAME,SIZE | grep "$device" | awk '{print $2}')
+	echo "Found these NVME disks /dev/$device: $size"
+	DISK_SIZE+=($(echo $size | cut -f1 -d"G"))
+done
+
+min_index=0
+
+for index in "${!DISK_SIZE[@]}"; do
+    if (( $(echo "${DISK_SIZE[$min_index]} > ${DISK_SIZE[$index]}" | bc -l) )); then
+	min_index=$index
+    fi
+done
+
+INSTALL_DISK="/dev/"${DISK[$min_index]}
+
+# INSTALL
+#
+
+parted $INSTALL_DISK -- mklabel gpt
+parted $INSTALL_DISK -- mkpart ESP fat32 1MB 512MB
+parted $INSTALL_DISK -- set 1 esp on
+parted $INSTALL_DISK -- mkpart root btrfs 512MB 100%
+
+mkfs.fat -F 32 -n boot /dev/$INSTALL_DISK"p1"
+mkfs.btrfs -L root /dev/$INSTALL_DISK"p2"
+
+mount /dev/$INSTALL_DISK"p2" /mnt
+mount --mkdir /dev/$INSTALL_DISK"p1" /mnt/boot
+
+pacstrap -K /mnt base linux linux-firmware
+
+packages=(
+    amd-ucode
+    git
+    mesa
+    lib32_mesa
+    xf86-video-amdgpu
+    vulkan-radeon
+    lib32-vulkan-radeon
+    libva-mesa-driver
+    lib32-libva-mesa-driver
+    neovim
+    htop
+    lm_sensors
+    pipewire
+    lib32-pipewire
+    networkmanager
+)
+
+for package in ${packages[@]}; do
+    echo "Installing $package"
+    yes | pacman -S "$package"
+done
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# CHROOT
+#
+
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/Europe/Prague /etc/localtime
+hwclock --systohc
+
+sed -i '/^# *en_US.UTF-8 UTF-8/s/^# *//' /etc/locale.gen
+locale-gen
+
+echo "LANG=en_US.UTF-8" | tee /etc/locale.conf
+echo "arch_laptop" | tee /etc/hostname
+
+# SERVICES
+#
+
+systemctl enable NetworkManager.service
+
+# APP SETTINGS
+#
+
+git config --global user.name  "Ardn0"
+git config --global user.email "holomek.o@gmail.com"
+
+passwd
+
+bootctl install
+exit
+umount -R /mnt
+reboot
